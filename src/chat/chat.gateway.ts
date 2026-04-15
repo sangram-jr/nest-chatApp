b2a987e134c75a8d313711034fa6f25bb6c4f3a2
sangram-jr <sangrambera58@gmail.com>
2026-04-15 13:09:51 +0530

    init ws server

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,40 @@
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+/*
+@WebSocketGateway(4000)
+export class ChatGateway {
+
+  @SubscribeMessage('events') //Listen for an event called "events"
+  handleEvent(@MessageBody() data:string,@ConnectedSocket() client: Socket){//@MessageBody() → gets data , data=msg sent from client ,@ConnectedSocket() → gets client, client=specific user's socket
+    console.log(data);
+    client.emit('events','this is a reply');//emit means send msg,   Sends response back to the same client
+  }
+}*/
+
+
+
+//msg broadcast to everyone including me those connected to same event -> ex. events
+/*
+@WebSocketGateway(4000)
+export class ChatGateway {
+
+  @WebSocketServer() server:Server //msg broadcast to everyone
+  @SubscribeMessage('events') //Listen for an event called "events"
+  handleEvent(@MessageBody() data:string,@ConnectedSocket() client: Socket){//@MessageBody() → gets data , data=msg sent from client ,@ConnectedSocket() → gets client, client=specific user's socket
+    
+    this.server.emit('events',data);//send the same data to all those are connected to particular event-> in this case my event name is "events"
+  }
+}*/
+
+
+//msg broadcast to everyone excluding me those connected to same event -> ex. events
+@WebSocketGateway(4000)
+export class ChatGateway {
+
+  
+  @SubscribeMessage('events') //Listen for an event called "events"
+  handleEvent(@MessageBody() data:string,@ConnectedSocket() client: Socket){//@MessageBody() → gets data , data=msg sent from client ,@ConnectedSocket() → gets client, client=specific user's socket
+    client.broadcast.emit("events",data); //emit means send msg,   Sends response back to the same client those are connected to same event-> in our case event is "events"
+  }
+}
