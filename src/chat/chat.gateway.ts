86775f30e76ada84ee5673b9a24af2cb146e5708
sangram-jr <sangrambera58@gmail.com>
2026-04-15 13:27:02 +0530

    add user connected and disconnected logic

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,4 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 /*
@@ -30,9 +30,19 @@ export class ChatGateway {
 
 //msg broadcast to everyone excluding me those connected to same event -> ex. events
 @WebSocketGateway(4000)
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection,OnGatewayDisconnect {
 
-  
+  //when connection established, then this method execute
+  //this method comes from OnGatewayConnection
+  handleConnection(client: Socket) {
+    console.log(`A new User joined with id ${client.id}`);
+  }
+  ////this method comes from OnGatewayDisconnect
+  //when connection broke, then this method execute
+  handleDisconnect(client: Socket) {
+    console.log(`User left with id ${client.id}`);
+  }
+
   @SubscribeMessage('events') //Listen for an event called "events"
   handleEvent(@MessageBody() data:string,@ConnectedSocket() client: Socket){//@MessageBody() → gets data , data=msg sent from client ,@ConnectedSocket() → gets client, client=specific user's socket
     client.broadcast.emit("events",data); //emit means send msg,   Sends response back to the same client those are connected to same event-> in our case event is "events"
